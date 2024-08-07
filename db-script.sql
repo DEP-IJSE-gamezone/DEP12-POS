0d671356746f2fa99f20cceade0c0804461c60bf
Sachith Rangana1 <sachith2546@gmail.com>
2024-08-07 19:05:01 +0530

    Add Order

--- db-script.sql
+++ db-script.sql
@@ -1,1 +1,8 @@
---please change this
+CREATE TABLE "order"(
+                        id VARCHAR(10) PRIMARY KEY ,
+                        date DATE NOT NULL,
+                        user_name VARCHAR(100) NOT NULL ,
+                        customer_id VARCHAR(10) NOT NULL ,
+                        CONSTRAINT fk_user FOREIGN KEY(user_name) REFERENCES "user"(username),
+                        CONSTRAINT fk_customer_id FOREIGN KEY (customer_id) REFERENCES customer(id)
+);
