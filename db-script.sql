4a2306b13d485b5ab845493996771ffd76b42ad2
LahiruHarshana <lharshana2002@gmail.com>
2024-08-07 19:04:09 +0530

    add-user-rows

--- db-script.sql
+++ db-script.sql
@@ -1,1 +1,12 @@
---please change this
+
+INSERT INTO user (username, name, password) VALUES
+                                                ('user1', 'Saman edirimuni', 'password1'),
+                                                ('user2', 'Sahan sena', 'password2'),
+                                                ('user3', 'Sandaru sankalpa', 'password3'),
+                                                ('user4', 'saman sena', 'password4'),
+                                                ('user5', 'Lahiru chankya', 'password5'),
+                                                ('user6', 'vimukthi chandrasena', 'password6'),
+                                                ('user7', 'Docktor sachith', 'password7'),
+                                                ('user8', 'Sanduni sapasara', 'password8'),
+                                                ('user9', 'dilani menakaa', 'password9'),
+                                                ('user10', 'sadalu merun', 'password10');
