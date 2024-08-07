cc4e7d6a9d760e23b71f46170a8acf7e347e3849
diliniapsara <deegaladil@gmail.com>
2024-08-07 19:04:56 +0530

    add user table

--- db-script.sql
+++ db-script.sql
@@ -1,1 +1,6 @@
---please change this
+
+CREATE TABLE  "user"(
+                        username VARCHAR(100) PRIMARY KEY ,
+                        name VARCHAR(200) NOT NULL ,
+                        password VARCHAR(300) NOT NULL
+);
